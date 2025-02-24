1f3dbeaedaa32c97ca2f73cbdf14da4049543424
Jesse Vang <jessevang@sbcglobal.net>
2025-02-23 17:43:09 -0800

    Updated and reworked

---
+++ MoreMonsterDrops.cs
@@ -0,0 +1,44 @@
+﻿
+using StardewModdingAPI;
+
+using Spacechase.Shared.Patching;
+
+
+namespace MoreMonsterDrops
+{
+
+
+    public class Config
+    {
+        public bool TurnOnMoreMonsterDrops { get; set; } = true;
+        public double NumberOfTimesDropFunctionExecutesPerCombatLevel { get; set; } = 0.5;
+        public int ExtraNumberOfTimesDropFunctionExecuteRegardlessOfCombatLevel { get; set; } = 5;
+    }
+
+
+    internal sealed class ModEntry : Mod
+    {
+
+        public static ModEntry Instance { get; private set; }
+        public Config Config { get; private set; }
+
+        public override void Entry(IModHelper helper)
+        {
+            Instance = this;
+            Config = helper.ReadConfig<Config>() ?? new Config();
+
+            //This Harmony Patcher turns on more monster drops
+            HarmonyPatcher.Apply(this,
+            new DropFunctionPatch(Config, Instance)
+            );
+
+        }
+
+
+
+
+
+
+    }
+}
+
